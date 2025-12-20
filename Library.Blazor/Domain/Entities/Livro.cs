c322c32da73192cd40fb6dd917c3ed9904723634
naki <leandroreisubi@gmail.com>
2025-12-20 12:05:47 -0300

    Adicionar arquivos de projeto.

---
+++ Library.Blazor/Domain/Entities/Livro.cs
@@ -0,0 +1,32 @@
+﻿namespace Library.Blazor.Domain.Entities
+{
+    public class Livro
+    {
+        public int Id { get; private set; }
+        public string Titulo { get; private set; }
+        public string Autor { get; private set; }
+        public bool Disponivel { get; private set; }
+
+        public Livro(int id, string titulo, string autor)
+        {
+            Id = id;
+            Titulo = autor;
+            Autor = autor;
+            Disponivel = true;
+        }
+
+        public void Emprestar()
+        {
+            if (!Disponivel)
+                throw new InvalidOperationException("Livro já emprestado");
+
+            Disponivel = false;
+        }
+
+        public void Devolver()
+        {
+            Disponivel = true;
+        }
+
+    }
+}
