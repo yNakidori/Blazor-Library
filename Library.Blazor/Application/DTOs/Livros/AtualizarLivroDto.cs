550fd61ec0366ddbda6b04635d4ae598567d6ab1
naki <leandroreisubi@gmail.com>
2025-12-30 22:31:18 -0300

    Adiciona DTOs para criação e atualização de livros
    Foram criados os arquivos AtualizarLivroDto.cs e CriarLivroDto.cs no namespace Library.Blazor.Application.DTOs.Livros, contendo as classes necessárias para manipulação de dados de livros nas operações de criação e atualização.

---
+++ Library.Blazor/Application/DTOs/Livros/AtualizarLivroDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Blazor.Application.DTOs.Livros
+{
+    public class AtualizarLivroDto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; }= string.Empty;
+    }
+}
---
+++ Library.Blazor/Application/DTOs/Livros/CriarLivroDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Blazor.Application.DTOs.Livros
+{
+    public class CriarLivroDto
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+    }
+}
